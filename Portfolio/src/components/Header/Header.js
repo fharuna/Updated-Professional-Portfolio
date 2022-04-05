ad41f34553686fe0503e1e6cb66e30fe572aa077
Fati Haruna <haruna.fati@gmail.com>
2022-04-05 08:03:07 -0400

    header changes

--- Portfolio/src/components/Header/Header.js
+++ Portfolio/src/components/Header/Header.js
@@ -6,9 +6,32 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <div1>
+    <Link href="/">
+      <a style={{display: "flex", alignItems: "center", color: 'white'}}>
+        <DiCssdeck size="3rem" /> <span>Portfolio</span>
+      </a>
+    </Link>
+  </div1>
+  <Div2>
+    <li>
+      <Link href="#project">
+      <NavLink>Projects</NavLink>
+      </Link>
+    </li>
+    <li>
+      <Link href="#tech">
+      <NavLink>Technologies Used</NavLink>
+      </Link>
+    </li>
+    <li>
+      <Link href="#about">
+      <NavLink>About Me</NavLink>
+      </Link>
+    </li>
+  </Div2>
+  </Container>
 );
 
 export default Header;
